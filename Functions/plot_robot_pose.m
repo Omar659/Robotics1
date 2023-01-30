16d64767f24b3795e4fa042055bd4688c6ae7218
Omar659 <bayoumi.omar97@gmail.com>
2023-01-30 15:22:01 +0100

    auto

--- Functions/plot_robot_pose.m
+++ Functions/plot_robot_pose.m
@@ -46,68 +46,187 @@ function plot_robot_pose(joint_types, DH_table, O_A_i)
     % Alpha channel
     face_alpha = 0.5;
 
-    t = linspace(0,1/2*pi);
-    v = [];
-    f = [];
-    for i = 1:length(t)-1
-        v1 = 0.7*[0 0 0];
-        v2 = 0.7*[cos(t(i)) sin(t(i)) 0*t(i)];
-        v3 = 0.7*[cos(t(i+1)) sin(t(i+1)) 0*t(i+1)];
-        v = [v; v1; v2; v3];
-        f_i = [((i-1)*3)+1 ((i-1)*3)+2 ((i-1)*3)+3];
-        f = [f; f_i];
-    end
-    rot = O_A_i(1:3, end-3:end-1);
-    tran = O_A_i(1:3, end);
-    v = double(vpa(simplify(rot*v' + tran), 4));
-    v = v';
-    patch(Faces = f, Vertices = v, FaceColor = [1 0 0], EdgeColor='none', FaceAlpha=face_alpha);
+    % Colors
+    x_color = [.7 0 0];
+    y_color = [0 .7 0];
+    z_color = [0 0 .7];
+    disp_color = [0 0.5 1];
+    comm_norm_color = [1 0.9 0.3];
 
+    % Plot parameters
+    line_width = 2;
+    font_size = 10;
+    
+    % Radii of the cylinders
+    radii_normal = ones(1, 22)*r_finish;
+    radii_normal = [0 radii_normal 0];
+    radii_prismatic_link = linspace(r_start, r_finish, 24);
+    radii_prismatic_link = [0 radii_prismatic_link(1:6) ...
+                            radii_prismatic_link(7:12)*0.9  ...
+                            radii_prismatic_link(13:18)*0.83  ...
+                            radii_prismatic_link(19:24)*0.77 0];
     view(3);
     hold on
+        % Joints plot
         for i = 2:size(joint_pos, 2)
-            radiuses = ones(1, 22)*r_finish;
-            radiuses = [0 radiuses 0];
+            % Radii for prismatic/revolute joint
+            radii = radii_normal;
             if joint_types(i-1) == "p"
-                radiuses = linspace(r_start, r_finish, 24);
-                radiuses = [0 radiuses(1:6) radiuses(7:12)*0.9 radiuses(13:18)*0.83 radiuses(19:24)*0.77 0];
+                radii = radii_prismatic_link;
             end
-            to = joint_pos(:, i-1) + DH_d(i-1)*joint_z(:, i-1);
-            [x_c, y_c, z_c] = cylinder2P(radiuses, 20, joint_pos(:, i-1)', to');
-            x_c = double(x_c);
-            y_c = double(y_c);
-            z_c = double(z_c);
-            surf(x_c, y_c, z_c, FaceColor=[0 0.5 1], EdgeColor='none', FaceAlpha=face_alpha)
 
-            radiuses = ones(1, 22)*r_finish;
-            radiuses = [0 radiuses 0];
-            to = joint_pos(:, i) - DH_a(i-1)*joint_x(:, i);
-            [x_c, y_c, z_c] = cylinder2P(radiuses, 20, joint_pos(:, i)', to');
+            % Displacement
+            to = joint_pos(:, i-1) + DH_d(i-1)*joint_z(:, i-1);
+            [x_c, y_c, z_c] = cylinder2P(radii, 20, joint_pos(:, i-1)', to');
             x_c = double(x_c);
             y_c = double(y_c);
             z_c = double(z_c);
-            surf(x_c, y_c, z_c, FaceColor=[1 0.9 0.3], EdgeColor='none', FaceAlpha=face_alpha)
+            surf(x_c, y_c, z_c, FaceColor=disp_color, EdgeColor='none', FaceAlpha=face_alpha)
+            disp_from = joint_pos(:, i-1) + 1.3*r_revolute*joint_x(:, i-1);
+            disp_to = joint_pos(:, i-1) + DH_d(i-1)*joint_z(:, i-1) + 1.3*r_revolute*joint_x(:, i-1);
+            plot3([disp_from(1) disp_to(1)], [disp_from(2) disp_to(2)], [disp_from(3) disp_to(3)], ...
+                  Color = disp_color, LineWidth=line_width)
+            text((disp_from(1) + disp_to(1))/2, (disp_from(2) + disp_to(2))/2, (disp_from(3) + disp_to(3))/2, ...
+                 strcat("d", num2str(i-1), "=", num2str(double(DH_d(i-1)))), ...
+                 Color = disp_color, FontSize=font_size, ...
+                 HorizontalAlignment="right", VerticalAlignment="middle",FontWeight="bold")
+
+            % Common normal
+            radii = radii_normal;
+            to = joint_pos(:, i) - DH_a(i-1)*joint_x(:, i);
+            [x_c, y_c, z_c] = cylinder2P(radii, 20, joint_pos(:, i)', to');
+            x_c = double(x_c);
+            y_c = double(y_c);
+            z_c = double(z_c);
+            surf(x_c, y_c, z_c, FaceColor=comm_norm_color, EdgeColor='none', FaceAlpha=face_alpha)
+            disp_from = joint_pos(:, i) + 1.3*r_revolute*joint_z(:, i);
+            disp_to = joint_pos(:, i) - DH_a(i-1)*joint_x(:, i) + 1.3*r_revolute*joint_z(:, i);
+            plot3([disp_from(1) disp_to(1)], [disp_from(2) disp_to(2)], [disp_from(3) disp_to(3)], ...
+                  Color = comm_norm_color, LineWidth=line_width)
+            text((disp_from(1) + disp_to(1))/2, (disp_from(2) + disp_to(2))/2, (disp_from(3) + disp_to(3))/2, ...
+                 strcat("a", num2str(i-1), "=", num2str(double(DH_a(i-1)))), ...
+                 Color = comm_norm_color, FontSize=font_size, ...
+                 HorizontalAlignment="right", VerticalAlignment="middle",FontWeight="bold")
         end
         for p = 1:size(joint_pos, 2)
             from = joint_pos(:, p);
             to_x = joint_x(:, p);
             to_y = joint_y(:, p);
             to_z = joint_z(:, p);
-            quiver3(from(1), from(2), from(3), to_x(1), to_x(2), to_x(3), Color = [1 0 0])
-            quiver3(from(1), from(2), from(3), to_y(1), to_y(2), to_y(3), Color = [0 1 0])
-            quiver3(from(1), from(2), from(3), to_z(1), to_z(2), to_z(3), Color = [0 0 1])
-            text(from(1) + to_x(1), from(2) + to_x(2), from(3) + to_x(3), strcat("x", num2str(p-1)))
-            text(from(1) + to_y(1), from(2) + to_y(2), from(3) + to_y(3), strcat("y", num2str(p-1)))
-            text(from(1) + to_z(1), from(2) + to_z(2), from(3) + to_z(3), strcat("z", num2str(p-1)))
+            quiver3(from(1), from(2), from(3), to_x(1), to_x(2), to_x(3), Color = x_color, LineWidth=line_width)
+            quiver3(from(1), from(2), from(3), to_y(1), to_y(2), to_y(3), Color = y_color, LineWidth=line_width)
+            quiver3(from(1), from(2), from(3), to_z(1), to_z(2), to_z(3), Color = z_color, LineWidth=line_width)
+            text(from(1) + to_x(1), from(2) + to_x(2), from(3) + to_x(3), ...
+                 strcat("x", num2str(p-1)), Color = x_color, FontSize=font_size, ...
+                 HorizontalAlignment="center", VerticalAlignment="middle",FontWeight="bold")
+            text(from(1) + to_y(1), from(2) + to_y(2), from(3) + to_y(3), ...
+                 strcat("y", num2str(p-1)), Color = y_color, FontSize=font_size, ...
+                 HorizontalAlignment="center", VerticalAlignment="middle",FontWeight="bold")
+            text(from(1) + to_z(1), from(2) + to_z(2), from(3) + to_z(3), ...
+                 strcat("z", num2str(p-1)), Color = z_color, FontSize=font_size, ...
+                 HorizontalAlignment="center", VerticalAlignment="middle",FontWeight="bold")
             
             if p > 1
-                from = joint_pos(:, p-1);
-                quiver3(from(1), from(2), from(3), to_x(1), to_x(2), to_x(3), Color = [1 0 0], LineStyle="--")
-                text(from(1) + to_x(1), from(2) + to_x(2), from(3) + to_x(3)-0.05, strcat("x", num2str(p-1)))
-                from = joint_pos(:, p);
-                to_z = joint_z(:, p-1);
-                quiver3(from(1), from(2), from(3), to_z(1), to_z(2), to_z(3), Color = [0 0 1], LineStyle="--")
-                text(from(1) + to_z(1), from(2) + to_z(2), from(3) + to_z(3)-0.05, strcat("z", num2str(p-2)))
+                from_x_j = joint_pos(:, p-1);
+                offset = 0;
+                if DH_th(p-1) == 0
+                    offset = 0.05;
+                end
+                quiver3(from_x_j(1), from_x_j(2), from_x_j(3), to_x(1), to_x(2), to_x(3), Color = x_color, LineStyle="--", LineWidth=line_width)
+                text(from_x_j(1) + to_x(1), from_x_j(2) + to_x(2), from_x_j(3) + to_x(3)-offset, ...
+                     strcat("x", num2str(p-1)), Color = x_color, FontSize=font_size, ...
+                     HorizontalAlignment="center", VerticalAlignment="middle",FontWeight="bold")
+                from_z_j = joint_pos(:, p);
+                to_z_j = joint_z(:, p-1);
+                offset = 0;
+                if DH_al(p-1) == 0
+                    offset = 0.05;
+                end
+                quiver3(from_z_j(1), from_z_j(2), from_z_j(3), to_z_j(1), to_z_j(2), to_z_j(3), Color = z_color, LineStyle="--", LineWidth=line_width)
+                text(from_z_j(1) + to_z_j(1), from_z_j(2) + to_z_j(2), from_z_j(3) + to_z_j(3)-offset, ...
+                     strcat("z", num2str(p-2)), Color = z_color, FontSize=font_size,...
+                     HorizontalAlignment="center", VerticalAlignment="middle",FontWeight="bold")
+
+                % Circle for theta
+                t = linspace(0,DH_th(p-1), 20);
+                v_th = [];
+                f_th = [];
+                v_line_th = [];
+                for i = 1:length(t)-1
+                    v1 = 0.7*[0 0 0];
+                    v2 = 0.7*[cos(t(i)) sin(t(i)) 0*t(i)];
+                    v3 = 0.7*[cos(t(i+1)) sin(t(i+1)) 0*t(i+1)];
+                    v_th = [v_th; v1; v2; v3];
+                    v_line_new = 0.7*[cos(t(i)) sin(t(i)) 0*t(i)];
+                    v_line_th = [v_line_th; v_line_new];
+                    f_i = [((i-1)*3)+1 ((i-1)*3)+2 ((i-1)*3)+3];
+                    f_th = [f_th; f_i];
+                end
+                v_line_new = 0.7*[cos(t(end)) sin(t(end)) 0*t(end)];
+                v_line_th = [v_line_th; v_line_new];
+                
+                rot = O_A_i(1:3, (p-2)*4 + 1:(p-2)*4 + 3);
+                tran = O_A_i(1:3, (p-2)*4 + 4);
+                v_th = double(vpa(simplify(rot*v_th' + tran), 4));
+                v_th = v_th';
+                v_line_th = double(vpa(simplify(rot*v_line_th' + tran), 4));
+                v_line_th = v_line_th';
+                patch(Faces = f_th, Vertices = v_th, FaceColor = x_color, EdgeColor='none', FaceAlpha=face_alpha/2);
+                text((joint_pos(1, p-1) + 0.7*joint_x(1, p-1) + from_x_j(1) + 0.7*to_x(1))/2, ...
+                     (joint_pos(2, p-1) + 0.7*joint_x(2, p-1) + from_x_j(2) + 0.7*to_x(2))/2, ...
+                     (joint_pos(3, p-1) + 0.7*joint_x(3, p-1) + from_x_j(3) + 0.7*to_x(3))/2, ...
+                     strcat("Θ", num2str(p-1), "=", num2str(double(DH_th(p-1)))), Color=x_color, FontSize=font_size, ...
+                     HorizontalAlignment="center", VerticalAlignment="middle",FontWeight="bold")
+                plot3(v_line_th(1:end-1,1), v_line_th(1:end-1,2), v_line_th(1:end-1,3), ...
+                      Color = x_color, LineWidth=line_width, LineStyle="--")
+                from = v_line_th(end-1,:);
+                to = v_line_th(end,:);
+                [x_c, y_c, z_c] = cylinder2P([0.03 0], 7, from, to);
+                x_c = double(x_c);
+                y_c = double(y_c);
+                z_c = double(z_c);
+                surf(x_c, y_c, z_c, FaceColor=x_color, EdgeColor='none')
+
+                
+                % Circle for alpha
+                t = linspace(0,DH_al(p-1), 20);
+                v_al = [];
+                f_al = [];
+                v_line_al = [];
+                for i = 1:length(t)-1
+                    v1 = 0.7*[0 0 0];
+                    v2 = 0.7*[0*t(i) sin(t(i)) cos(t(i))];
+                    v3 = 0.7*[0*t(i+1) sin(t(i+1)) cos(t(i+1))];
+                    v_al = [v_al; v1; v2; v3];
+                    v_line_new = 0.7*[0*t(i) sin(t(i)) cos(t(i))];
+                    v_line_al = [v_line_al; v_line_new];
+                    f_i = [((i-1)*3)+1 ((i-1)*3)+2 ((i-1)*3)+3];
+                    f_al = [f_al; f_i];
+                end
+                v_line_new = 0.7*[0*t(end) sin(t(end)) cos(t(end))];
+                v_line_al = [v_line_al; v_line_new];
+
+                rot = O_A_i(1:3, (p-1)*4 + 1:(p-1)*4 + 3);
+                tran = O_A_i(1:3, (p-1)*4 + 4);
+                v_al = double(vpa(simplify(rot*v_al' + tran), 4));
+                v_al = v_al';
+                v_line_al = double(vpa(simplify(rot*v_line_al' + tran), 4));
+                v_line_al = v_line_al';
+                patch(Faces = f_al, Vertices = v_al, FaceColor = z_color, EdgeColor='none', FaceAlpha=face_alpha/2);
+                text((joint_pos(1, p) + 0.7*joint_z(1, p) + joint_pos(1, p) + 0.7*to_z_j(1))/2, ...
+                     (joint_pos(2, p) + 0.7*joint_z(2, p) + joint_pos(2, p) + 0.7*to_z_j(2))/2, ...
+                     (joint_pos(3, p) + 0.7*joint_z(3, p) + joint_pos(3, p) + 0.7*to_z_j(3))/2, ...
+                     strcat("α", num2str(p-1), "=", num2str(double(DH_al(p-1)))), Color=z_color, FontSize=font_size, ...
+                     HorizontalAlignment="center", VerticalAlignment="middle",FontWeight="bold")
+                plot3(v_line_al(2:end,1), v_line_al(2:end,2), v_line_al(2:end,3), ...
+                      Color = z_color, LineWidth=line_width, LineStyle="--")
+                from = v_line_al(2,:);
+                to = v_line_al(1,:);
+                [x_c, y_c, z_c] = cylinder2P([0.03 0], 7, from, to);
+                x_c = double(x_c);
+                y_c = double(y_c);
+                z_c = double(z_c);
+                surf(x_c, y_c, z_c, FaceColor=z_color, EdgeColor='none')
             end
             if joint_types(p) == "ee"
                 from1 = joint_pos(:, p) - r_revolute*joint_y(:, p)*1.5;
